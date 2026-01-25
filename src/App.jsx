f05704b5f5da6d2169ee1c3dc49c61f1bdf95c75
Kenty4Real <kennethfurevikstrand@hotmail.com>
2026-01-25 13:40:55 +0100

    Coreconsept

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { CORE_CONCEPTS } from './data.js';
+import CoreConsept from './CoreConsept.jsx';
 
 function Header() {
   return(
@@ -22,11 +23,15 @@ function App() {
           <h2>Time to get started!</h2>
           <ul>
             <CoreConsept
-              title={CORE_CONCEPTS[0].title}
+             title={CORE_CONCEPTS[0].title}
               image={CORE_CONCEPTS[0].image}
               description={CORE_CONCEPTS[0].description}  
             />
-            <CoreConsept/>
+            <CoreConsept
+             title={CORE_CONCEPTS[1].title}
+              image={CORE_CONCEPTS[1].image}
+              description={CORE_CONCEPTS[1].description}  
+            />
             <CoreConsept/>
           </ul>
         </section>
---
+++ src/CoreConsept.jsx
@@ -0,0 +1,9 @@
+export default function CoreConsept() {
+    return(
+    <>
+        <h2>{title}</h2>
+        <img src={image} alt={title} />
+        <p>{description}</p>
+    </>
+    )
+}
